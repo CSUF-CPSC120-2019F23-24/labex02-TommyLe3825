074bb9daa435f6886d9bfb2a4278f1804697ebe9
TommyLe3825 <54484078+TommyLe3825@users.noreply.github.com>
2019-08-31 14:32:59 -0700

    Update main.cpp

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -6,17 +6,21 @@
 int main()
 {
   double mealcost,tax,tip,totalbill;
-
+  
   std::cout << "Welcome to the Restaurant Bill Calculator!\n";
+  //Input Meal Cost
   std::cout << "What is the total meal cost? ";
   std::cin >> mealcost;
-
+  
+  //Calculating Tax
   tax= mealcost * 0.0775;
   std::cout << "Tax:        $" << tax << "\n";
-
+  
+  //Calculating Tip
   tip= mealcost * 0.20;
   std::cout << "Tip:        $" << tip << "\n";
-
+  
+  //Calculating Total Bill
   totalbill=tip+tax+mealcost;
   std::cout << "Total Bill: $" << totalbill;
   return 0;
